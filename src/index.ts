78ec3554cb9d57b2eafd2e4ac7a07be319073b15
Mikhail Shardin <kriptonit1988@gmail.com>
2019-09-30 14:17:54 +0500

    Реализация функционала без полного использования TS

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import { router } from "./routes/login";
+import bodyParser from "body-parser";
+import cookieSession from "cookie-session";
+
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(cookieSession({
+  keys: ["xyz"]
+}))
+app.use(router);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
